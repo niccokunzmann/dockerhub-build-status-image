ae3e9920c7b44667af13dd719b92b3d6a9e5bd0b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-03-15 00:41:28 +0100

    add python server with GET /source

--- python-server/dockerhub_status_image_api.py
+++ python-server/dockerhub_status_image_api.py
@@ -2,9 +2,20 @@
 """
 
 """
-from bottle import run
-
+import sys
+import os
+from bottle import run, get, static_file
 
 __version__ = "0.0.0"
 __author__ = "Nicco Kunzmann"
 
+@get('/source')
+def static():
+    return static_file(os.path.basename(__file__),
+                       root=os.path.dirname(__file__))
+
+def main():
+    run(host='', port=80, debug=True)
+
+if __name__ == "__main__":
+    main()
